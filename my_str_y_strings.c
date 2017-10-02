8fb8701adfbe8fed0c8a90ac0adbf020123aa9f3
Kyle Lin <klin12@stuy.edu>
2017-10-02 15:08:20 -0400

    Initial Commit

---
+++ my_str_y_strings.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int mystrlen(char *) {
+
+}
+
+char * mystrncpy(char * dest, char * source, int n) {
+
+}
+
+int mystrcmp(char * s1, char * s2) {
+
+}
+
+char * mystrchr(char * s1, char * s2) {
+    
+}
+
+int main() {
+
+}
